3fdd006e9bf0c530d942a6e454f5f6ce3c1c6d08
Justin Carvalho <jstncarvalho@gmail.com>
2014-12-04 22:29:17 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 Pebble Text Watch With Date
 =================
 
-A tweak on the Text Watch. Watch face now reads full date in a small font in the bottom right corner. Can be configured to add "o'" or "oh" to minutes one through nine or inverted.
+A tweak on the Text Watch With Date. Watch face now reads displays battery percentage in the bottom left, and the weather in CURRENT (LOW/HIGH) CONDITIONS format in Celsius.
 
-Screenshot:  
-![Text watch with date screenshot](http://i.imgur.com/2XR1rfF.png)
+Available customizations:
+-Add o' to minutes 1-9
+-Add oh to minutes 1-9
+-Invert colors
+
+Screenshot:
+![Screenshot1](http://i.imgur.com/Z56h4cw.png)
+![Screenshot1](http://i.imgur.com/TBqnGrp.png)
--- src/TextWatch.c
+++ src/TextWatch.c
@@ -2,8 +2,12 @@
 #include <ctype.h>
 #include "num2words-en.h"
 
-#define DEBUG 0
 #define BUFFER_SIZE 44
+
+#define KEY_TEMPERATURE 0
+#define KEY_LOW 1
+#define KEY_HIGH 2
+#define KEY_CONDITIONS 3
 
 Window *window;
 
@@ -19,10 +23,13 @@ Line line2;
 Line line3;
 TextLayer *date;
 TextLayer *day;
+TextLayer *weather;
+TextLayer *battery;
 
 static char line1Str[2][BUFFER_SIZE];
 static char line2Str[2][BUFFER_SIZE];
 static char line3Str[2][BUFFER_SIZE];
+static char weatherString[32];
 
 static void destroy_property_animation(PropertyAnimation **prop_animation) {
     if (*prop_animation == NULL) {
@@ -40,25 +47,25 @@ static void destroy_property_animation(PropertyAnimation **prop_animation) {
 //Handle Date
 void setDate(struct tm *tm)
 {
-    static char dateString[] = "september 99th, 9999";
+    static char dateString[] = "september 99th";
     static char dayString[] = "wednesday";
     switch(tm->tm_mday)
     {
         case 1 :
         case 21 :
         case 31 :
-            strftime(dateString, sizeof(dateString), "%B %est, %Y", tm);
+            strftime(dateString, sizeof(dateString), "%B %est", tm);
             break;
         case 2 :
         case 22 :
-            strftime(dateString, sizeof(dateString), "%B %end, %Y", tm);
+            strftime(dateString, sizeof(dateString), "%B %end", tm);
             break;
         case 3 :
         case 23 :
-            strftime(dateString, sizeof(dateString), "%B %erd, %Y", tm);
+            strftime(dateString, sizeof(dateString), "%B %erd", tm);
             break;
         default :
-            strftime(dateString, sizeof(dateString), "%B %eth, %Y", tm);
+            strftime(dateString, sizeof(dateString), "%B %eth", tm);
             break;
     }
     strftime(dayString, sizeof(dayString), "%A", tm);
@@ -176,6 +183,80 @@ void display_initial_time(struct tm *t)
     setDate(t);
 }
 
+static void update_battery() {
+    static char battery_buffer[5] = "XX%";
+    BatteryChargeState charge_state = battery_state_service_peek();
+    snprintf(battery_buffer, sizeof(battery_buffer), "%d%%", charge_state.charge_percent);
+    text_layer_set_text(battery, battery_buffer);
+}
+
+void ask_for_weather() {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+    dict_write_end(iter);
+
+    // Send the message!
+    app_message_outbox_send();
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    static char temperature_buffer[8];
+    static char low_buffer[8];
+    static char high_buffer[8];
+    static char conditions_buffer[32];
+
+    Tuple *t = dict_read_first(iterator);
+
+    while (t != NULL) {
+        switch (t->key) {
+            case KEY_TEMPERATURE:
+                snprintf(temperature_buffer, sizeof(temperature_buffer), "%d", (int)t->value->int32);
+                break;
+            case KEY_LOW:
+                snprintf(low_buffer, sizeof(low_buffer), "%d", (int)t->value->int32);
+                break;
+            case KEY_HIGH:
+                snprintf(high_buffer, sizeof(high_buffer), "%d", (int)t->value->int32);
+                break;
+            case KEY_CONDITIONS:
+                snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+                break;
+            default:
+                APP_LOG(APP_LOG_LEVEL_ERROR, "App key: %d not found", (int)t->key);
+                break;
+        }
+        t = dict_read_next(iterator);
+    }
+
+    if (conditions_buffer[0]=='X') {
+        snprintf(weatherString, sizeof(weatherString), "X");
+    } else {
+        snprintf(weatherString, sizeof(weatherString), "%sC (%s/%s) %s", temperature_buffer, low_buffer, high_buffer, conditions_buffer);
+    }
+    text_layer_set_text(weather, weatherString);
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    snprintf(weatherString, sizeof(weatherString), "X");
+    text_layer_set_text(weather, weatherString);
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    snprintf(weatherString, sizeof(weatherString), "X");
+    text_layer_set_text(weather, weatherString);
+}
+
+void app_message_init() {
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
     #if INVERT
 // Configure the first line of text
 void configureBoldLayer(TextLayer *textlayer)
@@ -233,20 +314,20 @@ void init() {
     #endif
 
     // 1st line layers
-    line1.currentLayer = text_layer_create(GRect(0, 10, 144, 50));
-    line1.nextLayer = text_layer_create(GRect(144, 10, 144, 50));
+    line1.currentLayer = text_layer_create(GRect(0, 13, 144, 50));
+    line1.nextLayer = text_layer_create(GRect(144, 13, 144, 50));
     configureBoldLayer(line1.currentLayer);
     configureBoldLayer(line1.nextLayer);
 
     // 2nd layers
-    line2.currentLayer = text_layer_create(GRect(0, 47, 144, 50));
-    line2.nextLayer = text_layer_create(GRect(144, 47, 144, 50));
+    line2.currentLayer = text_layer_create(GRect(0, 50, 144, 50));
+    line2.nextLayer = text_layer_create(GRect(144, 50, 144, 50));
     configureLightLayer(line2.currentLayer);
     configureLightLayer(line2.nextLayer);
 
     // 3rd layers
-    line3.currentLayer = text_layer_create(GRect(0, 84, 144, 50));
-    line3.nextLayer = text_layer_create(GRect(144, 84, 144, 50));
+    line3.currentLayer = text_layer_create(GRect(0, 87, 144, 50));
+    line3.nextLayer = text_layer_create(GRect(144, 87, 144, 50));
     configureLightLayer(line3.currentLayer);
     configureLightLayer(line3.nextLayer);
 
@@ -262,9 +343,23 @@ void init() {
     text_layer_set_background_color(day, GColorClear);
     text_layer_set_font(day, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
     text_layer_set_text_alignment(day, GTextAlignmentRight);
+    //weather
+    weather = text_layer_create(GRect(0, 0, 144, 20));
+    text_layer_set_background_color(weather, GColorClear);
+    text_layer_set_text_color(weather, GColorBlack);
+    text_layer_set_font(weather, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text_alignment(weather, GTextAlignmentRight);
+    text_layer_set_text(weather, "loading...");
+    //battery
+    battery = text_layer_create(GRect(0, 150, 40, 168-150));
+    text_layer_set_background_color(battery, GColorClear);
+    text_layer_set_text_color(battery, GColorBlack);
+    text_layer_set_font(battery, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text_alignment(battery, GTextAlignmentLeft);
+    text_layer_set_text(battery, "XX%");
     #else
     //date & day layers
-    date = text_layer_create(GRect(0, 150, 144, 168-150));
+    date = text_layer_create(GRect(40, 150, 104, 168-150));
     text_layer_set_text_color(date, GColorWhite);
     text_layer_set_background_color(date, GColorClear);
     text_layer_set_font(date, fonts_get_system_font(FONT_KEY_GOTHIC_14));
@@ -274,6 +369,20 @@ void init() {
     text_layer_set_background_color(day, GColorClear);
     text_layer_set_font(day, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
     text_layer_set_text_alignment(day, GTextAlignmentRight);
+    //weather
+    weather = text_layer_create(GRect(0, 0, 144, 20));
+    text_layer_set_background_color(weather, GColorClear);
+    text_layer_set_text_color(weather, GColorWhite);
+    text_layer_set_font(weather, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text_alignment(weather, GTextAlignmentRight);
+    text_layer_set_text(weather, "loading...");
+    //battery
+    battery = text_layer_create(GRect(0, 150, 40, 168-150));
+    text_layer_set_background_color(battery, GColorClear);
+    text_layer_set_text_color(battery, GColorWhite);
+    text_layer_set_font(battery, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text_alignment(battery, GTextAlignmentLeft);
+    text_layer_set_text(battery, "XX%");
     #endif
 
     // Configure time on init
@@ -290,6 +399,12 @@ void init() {
     layer_add_child(window_layer, (Layer*)line3.nextLayer);
     layer_add_child(window_layer, (Layer*)date);
     layer_add_child(window_layer, (Layer*)day);
+    layer_add_child(window_layer, (Layer*)weather);
+    layer_add_child(window_layer, (Layer*)battery);
+
+    app_message_init();
+    ask_for_weather();
+    update_battery();
 
     tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
@@ -297,6 +412,7 @@ void init() {
 void deinit() {
 
     tick_timer_service_unsubscribe();
+    app_message_deregister_callbacks();
 
     layer_destroy((Layer*)line1.currentLayer);
     layer_destroy((Layer*)line1.nextLayer);
@@ -315,6 +431,9 @@ void deinit() {
     layer_destroy((Layer*)date);
     layer_destroy((Layer*)day);
 
+    layer_destroy((Layer*)weather);
+    layer_destroy((Layer*)battery);
+
     window_destroy(window);
 }
 
@@ -324,12 +443,14 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
     if (units_changed & DAY_UNIT) {
         setDate(tick_time);
     }
+    if (units_changed & HOUR_UNIT) {
+        ask_for_weather();
+        update_battery();
+    }
 }
 
 int main(void) {
-
     init();
     app_event_loop();
     deinit();
-
 }
